c5028b76225de8b40a1d54474dc1a051be1a7563
anhtn <tongngocanhcampha@gmail.com>
2022-07-16 08:30:25 +0700

    Login with google

--- src/App.js
+++ src/App.js
@@ -1,67 +1,57 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from 'react'
+import WeatherForestcast from './components/WeatherForestcast'
+import Login from './components/Login'
+import { auth, storeUserInfo } from './lib/firebase'
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [user, setUser] = useState([]);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      let newUser = null;
+      if (user) {
+        newUser = await storeUserInfo(user);
+      }
+      setUser(newUser);
+    });
+  }, []);
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
+  const HeaderContent = () => {
+    if (user) {
+      return (
+        <div class="navbar-end">
+          {/* <div class="navbar-item">
+            <Upload userImage={user.image} onSletctedImage={handleImageChanged} />
+            {user.name}
+          </div> */}
+          {/* <div class="navbar-item">
+            <button class="button is-danger is-light is-small" onClick={logout} > Logout</button>
+          </div> */}
+        </div >
+      )
+    } else {
+      return (<Login />)
     }
   }
 
+
+
   return (
-    <div className="app">
-      <div className="search">
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
+    <div className="">
+      <header class="navbar">
+        {/* {loading ? (
+          <p>
+            LOADING.....
+          </p>
+        ) : (
+          <HeaderContent />
+        )} */}
+        <HeaderContent />
+      </header >
+      <div>
+        {user && <WeatherForestcast />}
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{((data.main.temp.toFixed() - 32) / 1.8).toFixed(2)}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </div>
-
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        }
-
-
-
-      </div>
-    </div>
-  );
+    </div >
+  )
 }
 
 export default App;
---
+++ src/lib/firebase.js
@@ -0,0 +1,102 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import firebaseConfig from '../settings/firebase-config.json'
+
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+      location: user.location ? '' : user.location
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+      location: ''
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
